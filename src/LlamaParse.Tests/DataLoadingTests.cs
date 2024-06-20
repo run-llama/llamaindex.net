afdcbffc5e59e24d11f38eb7148a09830acfdff3
Diego Colombo <dicolomb@microsoft.com>
2024-06-20 19:22:42 +0100

    feat: Add tests for loading PDF files by page and including images
    This commit adds two new test cases to the `DataLoadingTests` class. The first test case (`can_load_pdf_file_by_page`) verifies that a PDF file can be loaded and split into individual pages. The second test case (`can_load_pdf_file_by_page_and_include_images`) extends the previous test by also checking if any images are included in the loaded documents.
    
    These tests use the `LlamaParseClient` class to load PDF files and assert that the resulting documents are not empty. Additionally, the second test asserts that at least one image document is present.
    
    The purpose of these tests is to ensure that the functionality for loading PDF files by page and including images works as intended.

--- src/LlamaParse.Tests/DataLoadingTests.cs
+++ src/LlamaParse.Tests/DataLoadingTests.cs
@@ -22,6 +22,42 @@ public class DataLoadingTests
     }
 
     [SkipOnKeyNotFoundFact]
+    public async Task can_load_pdf_file_by_page()
+    {
+        
+        var llamaParseClient = new LlamaParseClient(new HttpClient(new LoggingHandler(new HttpClientHandler())), Environment.GetEnvironmentVariable("LLAMA_CLOUD_API_KEY") ?? string.Empty);
+
+        var fileInfo = new FileInfo("./data/attention_is_all_you_need.pdf");
+
+        var documents = new List<Document>();
+        await foreach (var document in llamaParseClient.LoadDataAsync(fileInfo, splitByPage:true))
+        {
+            documents.Add(document);
+        }
+
+        documents.Should().NotBeEmpty();
+    }
+
+    [SkipOnKeyNotFoundFact]
+    public async Task can_load_pdf_file_by_page_and_include_images()
+    {
+        var configuration = new Configuration(extractImages: true);
+        var llamaParseClient = new LlamaParseClient(new HttpClient(new LoggingHandler(new HttpClientHandler())), Environment.GetEnvironmentVariable("LLAMA_CLOUD_API_KEY") ?? string.Empty, configuration:configuration);
+
+        var fileInfo = new FileInfo("./data/polyglot_tool.pdf");
+
+        var documents = new List<Document>();
+        await foreach (var document in llamaParseClient.LoadDataAsync(fileInfo, splitByPage: true))
+        {
+            documents.Add(document);
+        }
+
+        documents.Should().NotBeEmpty();
+
+        documents.OfType<ImageDocument>().Should().NotBeEmpty();
+    }
+
+    [SkipOnKeyNotFoundFact]
     public async Task can_load_pdf_powerpoint_file()
     {
         var llamaParseClient = new LlamaParseClient(new HttpClient(new LoggingHandler(new HttpClientHandler())), Environment.GetEnvironmentVariable("LLAMA_CLOUD_API_KEY") ?? string.Empty);
